e77cf52f292a78f8ce1e008d44e685877bcf8d13
custom3173 <bad2@umbc.edu>
2013-09-04 16:11:44 -0400

    Add configuration for smtp.umbc.edu emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,5 @@ Studentscheduler::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'gmail.com',
-      user_name:            'lits.notifications',
-      password:             'aoklcs53040',
-      authentication:       'plain',
       enable_starttls_auto: true  }
 end
