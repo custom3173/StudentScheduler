d420183e668c48d544fbe63a3be69c1e15856f44
David Bassett <dabassett.github@gmail.com>
2014-07-09 14:23:38 -0400

    Add datepickers and buttons icons
    Change date selection form to datepickers. Add button icons to edit
    and delete buttons for _schedule partial.

--- app/assets/javascripts/ui-stuff.js
+++ app/assets/javascripts/ui-stuff.js
@@ -3,40 +3,71 @@ jQuery.ajaxSetup({
 });
 
 $(function() {
-    /* UI related JS below here */
+  /* UI related JS below here */
 
-    // initialize jquery ui elements
-    $('button, input:submit, .button').button();
+  // initialize jquery ui elements
+  $('button, input:submit, .button').button();
 
-    // buttonsetify form elements (schedule form)
-    $('.jq-buttonset').buttonset();
+  // buttons w/ icons
+  $('a.edit').button({
+    icons: {
+      primary: "ui-icon-document"
+    }
+  });
 
-    // buttonsetify days on schedule form
+  $('a.delete').button({
+    icons: {
+      primary: "ui-icon-trash"
+    }
+  });
 
-    // accordionize the students' schedules lists
-    $(".schedule-accordion").accordion({
-        collapsible: true,
-        active: false
-    });
+  // buttonsetify form elements (schedule form)
+  $('.jq-buttonset').buttonset();
 
-    // use datepicker for calender date selection
-    $('#calendar-date').datepicker({
-        dateFormat: "yy-mm-dd"
-    });
+  // use datepickers for schedule form dates
+  $( "#schedule_start_date" ).datepicker({
+    dateFormat: "yy-mm-dd",
+    defaultDate: "+1w",
+    changeMonth: true,
+    numberOfMonths: 3,
+    onClose: function( selectedDate ) {
+      $( "#schedule_end_date" ).datepicker( "option", "minDate", selectedDate );
+    }
+  });
+  $( "#schedule_end_date" ).datepicker({
+    dateFormat: "yy-mm-dd",
+    defaultDate: "+1w",
+    changeMonth: true,
+    numberOfMonths: 3,
+    onClose: function( selectedDate ) {
+      $( "#schedule_start_date" ).datepicker( "option", "maxDate", selectedDate );
+    }
+  });
 
-    // add tooltips to the calendar
-    $(document).tooltip();
+  // accordionize the students' schedules lists
+  $(".schedule-accordion").accordion({
+    collapsible: true,
+    active: false
+  });
 
-    // hide the weekend schedules in the calendar
-    $('.weekend').hide();
-    $('#rows-container').width('770px');
-    $('#Show_weekend_schedules').change(function() {
-        if (this.checked) {
-            $('.weekend').show();
-            $('#rows-container').width('1024px');
-        } else {
-            $('.weekend').hide();
-            $('#rows-container').width('770px');
-        }
-    });
+  // use datepicker for calender date selection
+  $('#calendar-date').datepicker({
+    dateFormat: "yy-mm-dd"
+  });
+
+  // add tooltips to the calendar
+  $(document).tooltip();
+
+  // hide the weekend schedules in the calendar
+  $('.weekend').hide();
+  $('#rows-container').width('770px');
+  $('#Show_weekend_schedules').change(function() {
+    if (this.checked) {
+      $('.weekend').show();
+      $('#rows-container').width('1024px');
+    } else {
+      $('.weekend').hide();
+      $('#rows-container').width('770px');
+    }
+  });
 });
